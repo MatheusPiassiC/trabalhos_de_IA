bb7690c398025633fdce2e3485f5dcb1d443f031
piassi <matheuspiassi4@gmail.com>
2025-09-03 14:31:50 -0300

    knn manual funcional

--- knn_project/knn/core.py
+++ knn_project/knn/core.py
@@ -1,33 +1,29 @@
 import pandas
-
-def carregar_csv(filepath):
-    df = pandas.read_csv(filepath) #cria um dataframe
-    if "Id" in df.columns:
-        df = df.drop(columns=["Id"])
-    X = df.drop(columns=["Species"]) #atributos
-    y = df["Species"]  #espécies
-    #print(df.head())
-    return X,y
-
-X, y = carregar_csv("data/Iris.csv")
-df_completo = pandas.concat([X,y], axis=1)
-
-#print(X.head())
-#print(y.head())
-
-#print(X[:])
-#print(y[:])
+import utils 
 
 
-random_state = 30 #definindo uma seed para replicabilidade
-train_frac = 0.7 #porcentagem usada para treino
 
-df_embaralhado = df_completo.sample(frac=1, random_state=random_state).reset_index(drop=True) 
-#retorna todas as linhas embaralhadas
-#usando o random_state para replicar se necessário
-#também reinicia os indices
+df = utils.carregar_csv("../data/Iris.csv")
+X_train, y_train, X_test, y_test = utils.train_test_split_simple(df)
 
-train_size = int(train_frac*len(df_embaralhado))
+# print(X_train[:5])
+# print(y_train[:5])
+# print(X_test[:5])
+# print(y_test[:5])
 
+min_max = utils.calcular_min_max_params(X_train) #calcula apenas para o df de treino
+X_train_scaled = utils.aplicar_min_max_scaling(X_train, min_max)
+X_test_scaled = utils.aplicar_min_max_scaling(X_test, min_max)
+
+#print(X_train_scaled[:5])
+#print(X_test_scaled[:5])
+
+# print(X_train_scaled.dtypes)
+# print(X_test_scaled.dtypes)
+
+k = 3
+pred = utils.knn_predict(X_train_scaled, y_train, X_test_scaled, k)
+print(pred[:5])
+print(y_test[:5])
 
 
